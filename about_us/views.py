80530e6361f881329850688734f4e91f5ad6405d
Luis Monteiro <felipemoreno9065@gmail.com>
2023-08-16 22:34:52 -0300

    Endpoint terms of use created

--- about_us/views.py
+++ about_us/views.py
@@ -6,8 +6,8 @@ from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny
 
-from .models import AbousUs
-from .serializers import AboutUsSerializer
+from .models import AbousUs, TermsOfUse
+from .serializers import AboutUsSerializer, TermsOfUseSerializers
 
 
 class AboutUsViewSet(ModelViewSet):
@@ -22,6 +22,19 @@ class AboutUsViewSet(ModelViewSet):
             if about_us != None:
                 serializer = AboutUsSerializer(about_us)
                 return Response({'message': 'Sucesso', 'about_us': serializer.data}, status.HTTP_200_OK)
+            else:
+                return Response({'message': 'Nenhuma informação encontrada'}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as error:
+            print(error)
+            return Response({'message': 'Erro'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    @action(detail=False, methods=['GET'], permission_classes=[AllowAny])
+    def terms_of_use(self, request):
+        try:
+            terms = TermsOfUse.objects.last()
+            if terms != None:
+                serializer = TermsOfUseSerializers(terms)
+                return Response({'message': 'Sucesso', 'termos_of_use': serializer.data}, status=status.HTTP_200_OK)
             else:
                 return Response({'message': 'Nenhuma informação encontrada'}, status=status.HTTP_404_NOT_FOUND)
         except Exception as error:
