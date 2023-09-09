72cdf866da57a63d3d0e8855dcbd3892ba2818db
Luis Monteiro <felipemoreno9065@gmail.com>
2023-09-08 23:57:39 -0300

    Fixed validation in schedules cut

--- barber_shop/views.py
+++ barber_shop/views.py
@@ -262,7 +262,7 @@ class SchedulesViewset(ModelViewSet):
             # date_msg = datetime.strftime(data_obj, '%d/%m/%Y às %H:%M')
             # instance = UserProfile.objects.get(pk=data['chosen_barber_id'])
             # subject = 'BarberShop'
-            # message = f'O cliente {user} fez um novo agendamento para o dia {date_msg}'
+            # message = f'O cliente {user.username} fez um novo agendamento para o dia {schedule.day} {date_msg}'
             # send_email(instance.email, subject, message)
 
             return Response({'message': 'Agendamento feito com sucesso'}, status=status.HTTP_200_OK)
