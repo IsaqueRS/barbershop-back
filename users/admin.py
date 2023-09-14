4bed956405523dc87688a422d92d75685adacbec
Luis Monteiro <felipemoreno9065@gmail.com>
2023-09-14 14:32:19 -0300

    Barbers added in admin pt2

--- users/admin.py
+++ users/admin.py
@@ -28,6 +28,8 @@ class UserProfileAdmin(UserAdmin):
 
 class BarbersAdmin(admin.ModelAdmin):
     list_display = ['company', 'barber', 'email_barber']
+    list_filter = ['company']
+    search_fields = ['barber__username']
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == 'barber':
