6e160a3cb9d5290e410cf7d69711207083cbe7bb
Luis Monteiro <felipemoreno9065@gmail.com>
2023-10-01 13:47:45 -0300

    Endpoint of prices by barber created

--- prices/views.py
+++ prices/views.py
@@ -13,6 +13,19 @@ class PricesViewSet(ModelViewSet):
     serializer_class = PricesSerializers
     permission_classes = [IsAuthenticated]
 
+
+    @action(detail=False, methods=['GET'], permission_classes=[IsAuthenticated])
+    def list_prices_by_barber(self, request):
+        params = request.query_params
+        try:
+            prices = Prices.objects.filter(barber__barber_id=params['barber_id']).order_by('cut_price')
+            serializer = PricesSerializers(prices, many=True)
+            return Response({'message': 'Preços encontrados', 'prices': serializer.data}, status=status.HTTP_200_OK)
+        except Exception as error:
+            print(error)
+            return Response({'message': 'Erro ao listar os preços de corte do barbeiro'},
+                            status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
     @action(detail=False, methods=['GET'], permission_classes=[IsAuthenticated])
     def list_all_prices(self, request):
         try:
