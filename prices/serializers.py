28a7cb705aa2f6aae637052066a17dd20739f717
Luis Monteiro <felipemoreno9065@gmail.com>
2023-10-01 13:47:15 -0300

    Model of serializers fixed

--- prices/serializers.py
+++ prices/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import Prices
 
 
-class PricesSerializers(serializers.Serializer):
+class PricesSerializers(serializers.ModelSerializer):
     class Meta:
         model = Prices
         fields = '__all__'
