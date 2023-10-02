9559afe11d60fd8e32e5231dc10ec4e1f365a359
Luis Monteiro <felipemoreno9065@gmail.com>
2023-10-02 14:22:30 -0300

    Fixed fieldsets

--- prices/admin.py
+++ prices/admin.py
@@ -3,6 +3,9 @@ from .models import Prices
 
 
 class PricesAdmin(admin.ModelAdmin):
+    fieldsets = (
+        ('Informações sobre o corte', {'fields': ('barber', 'cut_price', 'cut_description', 'cut_photo')}),
+    )
     list_display = ['id', 'barber', 'cut_price']
     list_display_links = ['id', 'barber']
 
