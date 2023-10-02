8d4cf72056d728f2cdaf11d30902c9ee2ea4abbc
Luis Monteiro <felipemoreno9065@gmail.com>
2023-10-02 14:20:30 -0300

    Fix links

--- prices/admin.py
+++ prices/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from .models import Prices
 
+
 class PricesAdmin(admin.ModelAdmin):
     list_display = ['id', 'barber', 'cut_price']
+    list_display_links = ['id', 'barber']
+
 
 admin.site.register(Prices, PricesAdmin)
