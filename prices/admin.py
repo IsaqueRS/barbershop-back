1410f877f691c1a1d0b21da74bbebf094a8ce66e
Luis Monteiro <felipemoreno9065@gmail.com>
2023-10-01 22:49:48 -0300

    Dashboard of prices added in admin

--- prices/admin.py
+++ prices/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import Prices
 
-admin.site.register(Prices)
+class PricesAdmin(admin.ModelAdmin):
+    list_display = ['id', 'barber', 'cut_price']
+
+admin.site.register(Prices, PricesAdmin)
