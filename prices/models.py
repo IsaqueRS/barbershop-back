fb5c4b79127f28846c2511b6f3e7b2de960cc13a
Luis Monteiro <felipemoreno9065@gmail.com>
2023-10-05 22:38:45 -0300

    Field fixed

--- prices/models.py
+++ prices/models.py
@@ -1,15 +1,15 @@
 from django.db import models
-from users.models import Barbers
+# from users.models import Barbers
 
 
 class Prices(models.Model):
-    barber = models.ForeignKey(Barbers, verbose_name='Barbeiro', on_delete=models.CASCADE)
+    barber = models.ForeignKey('users.Barbers', verbose_name='Barbeiro', on_delete=models.CASCADE, null=True, blank=True)
     cut_price = models.FloatField('Preço do corte')
     cut_description = models.CharField('Tipo do corte')
     cut_photo = models.ImageField('Foto do corte', default='')
 
     def __str__(self):
-        return str(f'{self.barber} - {self.cut_price}')
+        return str(f'{self.cut_description} - {self.cut_price}')
 
     class Meta:
         verbose_name = 'Preço do corte'
