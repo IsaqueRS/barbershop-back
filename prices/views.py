67055a02effb688e7b1f80066d1fba97ef42782a
Luis Monteiro <felipemoreno9065@gmail.com>
2023-10-08 16:04:40 -0300

    Fix ident code and response

--- prices/views.py
+++ prices/views.py
@@ -109,13 +109,20 @@ class PricesViewSet(ModelViewSet):
             prices = Prices.objects.filter()
             if sort_by == 'price_asc':
                 price = prices.order_by('cut_price')
+                serializer = PricesSerializers(price, many=True)
+                return Response({'message': 'Preços encontrados', 'prices': serializer.data}, status=status.HTTP_200_OK)
+
             elif sort_by == 'price_desc':
                 price = prices.order_by('-cut_price')
+                serializer = PricesSerializers(price, many=True)
+                return Response({'message': 'Preços encontrados', 'prices': serializer.data}, status=status.HTTP_200_OK)
+
             elif sort_by == '' or ' ' or None:
                 price = prices.order_by('id')
+                serializer = PricesSerializers(price, many=True)
+                return Response({'message': 'Preços encontrados', 'prices': serializer.data}, status=status.HTTP_200_OK)
 
-            serializer = PricesSerializers(price, many=True)
-            return Response({'message': 'Preços encontrados', 'prices': serializer.data}, status=status.HTTP_200_OK)
         except Exception as error:
             print(error)
             return Response({'message': 'Erro ao listar preços!'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
