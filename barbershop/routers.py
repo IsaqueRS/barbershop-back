a2b0302023c4b195224cca431a40ae533e428004
Luis Monteiro <felipemoreno9065@gmail.com>
2023-10-11 22:24:53 -0300

    Fix ViewSet in routers

--- barbershop/routers.py
+++ barbershop/routers.py
@@ -1,7 +1,7 @@
 from rest_framework import routers
 
 from users.views import UserViewset, BarberViewSet
-from barber_shop.views import CompanysViewSet, SchedulesViewset
+from barber_shop.views import CompanysViewSet, SchedulesViewSet
 from about_us.views import AboutUsViewSet
 from prices.views import PricesViewSet
 
@@ -11,5 +11,5 @@ router.register(r'users', UserViewset, basename='users')
 router.register(r'barbers', BarberViewSet, basename='barbers')
 router.register(r'prices', PricesViewSet, basename='prices')
 router.register(r'companys', CompanysViewSet, basename='companys')
-router.register(r'schedules', SchedulesViewset, basename='schedules')
+router.register(r'schedules', SchedulesViewSet, basename='schedules')
 router.register(r'about_us', AboutUsViewSet, basename='about_us')
