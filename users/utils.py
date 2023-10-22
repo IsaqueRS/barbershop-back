c6d53db96d3e3dfb42946b48cc972c9084706ff0
Luis Monteiro <felipemoreno9065@gmail.com>
2023-10-22 19:56:53 -0300

    Function of send password from barber added in utils of users

--- users/utils.py
+++ users/utils.py
@@ -1,7 +1,12 @@
-
+import secrets
 
 def get_unique_or_none(model, *args, **kwargs):
     try:
         return model.objects.get(*args, **kwargs)
     except (model.DoesNotExist, model.MultipleObjectsReturned):
         return None
+
+def generate_random_password():
+    alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    password = ''.join(secrets.choice(alphabet) for i in range(12))
+    return password
